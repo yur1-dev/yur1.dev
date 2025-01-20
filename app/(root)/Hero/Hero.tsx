506466f3c69394a7c0af8d865bb5c2ab466dfb09
yur1-dev <yuriesb01@gmail.com>
2025-01-20 22:58:23 +0800

    update-1

--- app/(root)/Hero/Hero.tsx
+++ app/(root)/Hero/Hero.tsx
@@ -33,7 +33,7 @@ const Hero: React.FC = () => {
               <div>
                 <Button className="rounded bg-secondary text-white">
                   <a className="text-sm font-semibold" href="">
-                    Hire me
+                    Hire me1
                   </a>
                 </Button>
               </div>
