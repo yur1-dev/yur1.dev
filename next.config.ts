35ce6797d171f985555528a3f05dd955cc860fc6
yur1-dev <yuriesb01@gmail.com>
2026-05-07 20:15:00 +0800

    up

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.yur1.xyz" }],
+        destination: "https://yur1.xyz/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
