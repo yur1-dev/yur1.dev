4b5dc521e0ca0dc519fd2194833a8914b4c57024
yur1-dev <yuriesb01@gmail.com>
2026-05-08 18:45:08 +0800

    up sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,9 +4,15 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://yur1.xyz",
-      lastModified: new Date(),
+      lastModified: new Date("2026-05-07"),
       changeFrequency: "monthly",
       priority: 1,
+    },
+    {
+      url: "https://yur1.xyz/projects",
+      lastModified: new Date("2026-05-07"),
+      changeFrequency: "monthly",
+      priority: 0.8,
     },
   ];
 }
